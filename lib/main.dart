ca425c16394d930a02c3fb5d329d8359adf75147
Charin4 <charin4200@gmail.com>
2022-01-17 15:37:21 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,84 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+      Myapp()
+
   );
 }
+class Myapp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/chr.png') ,
+
+                ),
+                Text(
+                  'chrben69',
+                  style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.red
+                  ),
+                ),
+                Text(
+                  'DESIGNER',
+                  style: TextStyle(
+                    color: Colors.pink,
+                    letterSpacing:1.5,
+                  ),
+                ),
+                Card(
+
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                  child: Row(
+                    children: <Widget> [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.black,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('+213 420 69 69')
+                    ],
+
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(25.0),
+                    child: Row(
+                      children: <Widget> [
+                        Icon(Icons.email,
+                          color: Colors.black
+                          ,),
+                        SizedBox(width: 10.0,),
+                        Text(
+                          'chrben69@gmail.com',
+                          style: TextStyle(
+                            fontSize: 20.0,
+
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
+        ),
+      ),
+    );
+  }
+}
+
